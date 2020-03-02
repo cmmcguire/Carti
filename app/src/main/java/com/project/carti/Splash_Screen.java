040a53ad1c6d7a3de76a3c6521659034761c349a
Jeo16b <jose.o316@yahoo.com>
2020-03-02 17:52:19 -0500

    added the splash screen files

---
+++ app/src/main/java/com/project/carti/Splash_Screen.java
@@ -0,0 +1,28 @@
+package com.project.carti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash_Screen.this,MainActivity.class);
+                Splash_Screen.this.startActivity(mainIntent);
+                Splash_Screen.this.finish();
+            }
+        }, 3*1000);
+    }
+}
